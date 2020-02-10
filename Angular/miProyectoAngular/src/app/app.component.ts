8d14be64f5a3a924f2be86c87e597b733501f60f
unknown <Toni@LENI20180053.upvnet.upv.es>
2020-02-10 13:30:54 +0100

    Primer componente angular añadido

--- Angular/miProyectoAngular/src/app/app.component.ts
+++ Angular/miProyectoAngular/src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'miProyectoAngular';
+  nombre:string= "Toni";
+  apellidos:string= "Jordan rodriguez";
 }
