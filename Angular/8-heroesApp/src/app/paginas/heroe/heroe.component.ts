4bdc436cd709f132cb2079c354f00c4f83afffed
unknown <Toni@LENI20180053.upvnet.upv.es>
2020-02-24 17:11:39 +0100

    Practicando con sweetAlert

--- Angular/8-heroesApp/src/app/paginas/heroe/heroe.component.ts
+++ Angular/8-heroesApp/src/app/paginas/heroe/heroe.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { HeroeModel } from 'src/app/models/heroe.model';
 import { NgForm } from '@angular/forms';
 import { HeroesService } from 'src/app/services/heroes.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-heroe',
@@ -24,11 +25,73 @@ export class HeroeComponent implements OnInit {
       return;
     }
 
-    this.heroesService.crearHeroe(this.heroe).subscribe(resp=>{
-      console.log(resp);
+
+    if(this.heroe.id){
+      this.heroesService.actualizarHeroe(this.heroe).subscribe(resp=>{
+        console.log(resp);
+      })
+    }else{
+      this.heroesService.crearHeroe(this.heroe).subscribe(resp=>{
+        console.log(resp);
+      });
+    }
+  }
+
+
+
+  chupaMelaBasico(){
+    //Sweet alert basico
+    Swal.fire("Sweet Alert To guapa");
+  }
+
+  //En iconos de imagen tenemos los de: (error, success, info) (todas las clases de bootstrap de colores)
+  chupaMela2(){
+    Swal.fire({
+      icon: 'success',
+      title: 'Mensaje de error',
+      text: 'Este es un sweet alert de prueba'
     });
-
-
+  }
+  //Swal con imagen
+  chupaMela3(){
+    Swal.fire({
+      title: 'Thor vs Jormungand',
+      imageUrl: 'https://supercurioso.com/wp-content/uploads/2018/07/J%C3%B6rmundgander.jpg',
+      imageHeight: 500,
+      imageAlt: 'A tall image'
+    });
+  }
+  chupaMela4(){
+    Swal.fire({
+      title:'Sweet desde otra posicion',
+      icon: 'warning',
+      position: 'bottom-right'
+    });
+  }
+  //ShowClass para las animaciones del popup al entrar y hideClass pues al revés
+  chupaMela5(){
+    Swal.fire({
+      title:'Sweet con animate CSS ',
+      icon: 'info',
+      showClass: {
+        popup: 'animated fadeInDown faster'
+      },
+      hideClass: {
+        popup: 'animated fadeOutUp faster'
+      }
+    });
+  }
+  chupaMela6(){
+    Swal.fire({
+      title:'Sweet con animate CSS ',
+      icon: 'info',
+      showClass: {
+        popup: 'animated fadeInDown faster'
+      },
+      hideClass: {
+        popup: 'animated fadeOutUp faster'
+      }
+    });
   }
 
 }
--- Angular/8-heroesApp/src/app/services/heroes.service.ts
+++ Angular/8-heroesApp/src/app/services/heroes.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HeroeModel } from '../models/heroe.model';
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,25 @@ export class HeroesService {
 
   crearHeroe(heroe: HeroeModel){
     //Realizamos una consulta a una base de datos creada en firebase, primer acgumento el json, segundo argumento objeto donde se escribe el JSON
-    return this.http.post(`${this.url}/heroes.json`, heroe);
+    //2) pipe()función en RxJS : puede usar tuberías para unir operadores. Los tubos le permiten combinar múltiples funciones en una sola función
+    //map transforma lo que un observador puede regresr
+    return this.http.post(`${this.url}/heroes.json`, heroe)
+    .pipe(map((resp:any)=>{
+      heroe.id= resp.name;
+      return heroe;
+    }));
+  }
+
+  actualizarHeroe(heroe:HeroeModel){
+    //IMPORTANTE
+    //No podemos igual un objeto a otro en JS lo que se hace es copiar la referencia
+    //Por ello usamos:
+    const heroeTemp= {
+      ...heroe
+    };
+    delete heroeTemp.id;
+    console.log(heroeTemp);
+    return this.http.put(`${this.url}/heroes/${heroe.id}.json`, heroeTemp);
   }
 
 }
